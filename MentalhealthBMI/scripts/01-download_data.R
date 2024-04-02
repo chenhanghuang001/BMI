454432df6d6920a57a389d522cbf3b1e0deeefb5
chenhanghuang001 <165746567+chenhanghuang001@users.noreply.github.com>
2024-04-01 23:14:37 -0400

    Add files via upload

---
+++ MentalhealthBMI/scripts/01-download_data.R
@@ -0,0 +1,30 @@
+#### Preamble ####
+# Purpose: Downloads and saves the data 
+# Author: Chenhang Huang
+# Date: 21 Mar 2024
+# Contact: chenhang.huang@mail.utoronto.ca
+# License: UOT
+# Pre-requisites: no
+# data source: 
+
+
+
+#### Workspace setup ####
+library(tidyverse)
+
+#### Download data ####
+cchs = read.csv("data/raw_data/cchs-82M0013-E-2017-2018-Annual.csv",header = TRUE)
+
+set.seed(302)
+
+
+sampled_data <- cchs[sample(nrow(cchs), 5000), ]
+
+the_raw_data <- sampled_data |>select(GENDVSWL,INCDGHH,DHH_SEX,DHHGMS,DHH_OWN,HWTDGCOR) |> na.omit()
+
+#### Save data ####
+# [...UPDATE THIS...]
+# change the_raw_data to whatever name you assigned when you downloaded it.
+write_csv(the_raw_data, "data/raw_data/raw_data.csv") 
+
+         
